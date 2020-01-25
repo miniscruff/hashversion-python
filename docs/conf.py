6c7c76c896fdf1ff7920cdaf2987178130a98e0c
Ronnie Smith <halfpint1170@gmail.com>
2020-01-24 22:35:07 -0800

    Black the sphinx config file

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,7 @@
-project = 'HashVer Python'
-copyright = '2020, Ronnie Smith'
-author = 'Ronnie Smith'
+project = "HashVer Python"
+copyright = "2020, Ronnie Smith"
+author = "Ronnie Smith"
 
-extensions = [
-    "m2r",
-]
-exclude_patterns = ['_build']
-html_theme = 'sphinx_rtd_theme'
+extensions = ["m2r"]
+exclude_patterns = ["_build"]
+html_theme = "sphinx_rtd_theme"
